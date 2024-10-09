a423b451dae6ea31b240cc1aa631722b1a64c603
FelixTerres <fterres20935@iesjoanramis.org>
2024-10-09 13:24:04 +0200

    Add files via upload

---
+++ CV/js/script.js
@@ -0,0 +1,20 @@
+function canviaNom(){
+    let nombre = document.getElementById("name").value;
+    document.getElementById("nom").innerHTML= nombre;
+}
+
+function canviaColor(){
+    let color = document.getElementById("color").value;
+    document.body.style.color = color;
+}
+
+function canviaFons(){
+    let fons = document.getElementById("fons").value;
+    document.body.style.backgroundColor = fons;
+}
+
+function canviaFoto(){
+    let img = document.getElementById("foto").in
+    alert(img)
+    document.getElementById("foto").src = img ;
+}
